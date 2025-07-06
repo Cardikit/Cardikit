81b13a5ef2425044d1e0ea7257ce05905d155a66
Damion Voshall <126650414+DamoFD@users.noreply.github.com>
2025-07-06 13:51:41 -0700

    17 commands (#18)
    * run commands in container if container is in use
    
    * fixed error when running migrations on tables that exist

--- database/migrations/create_users_table.php
+++ database/migrations/create_users_table.php
@@ -2,11 +2,17 @@
 
 use App\Core\Migration;
 
+/**
+* Create the users table with specified columns.
+* Contains up and down methods for migration commands.
+*
+* @since 0.0.1
+*/
 return new class extends Migration {
     public function up(): void
     {
         $this->execute("
-            CREATE TABLE users (
+            CREATE TABLE IF NOT EXISTS users (
                 id INT AUTO_INCREMENT PRIMARY KEY,
                 name VARCHAR(255),
                 email VARCHAR(255),
@@ -18,6 +24,6 @@ return new class extends Migration {
 
     public function down(): void
     {
-        $this->execute("DROP TABLE users");
+        $this->execute("DROP TABLE IF EXISTS users");
     }
 };
