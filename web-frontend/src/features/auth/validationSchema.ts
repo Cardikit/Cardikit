3657700479018ae64e6e775b56958baf7de09828
DamoFD <voshall@yahoo.com>
2025-07-10 20:44:32 -0700

    auth pages tests

--- web-frontend/src/features/auth/validationSchema.ts
+++ web-frontend/src/features/auth/validationSchema.ts
@@ -8,10 +8,10 @@ import * as yup from 'yup';
 * @since 0.0.1
 */
 export const registerSchema = yup.object({
-    name: yup.string().min(2, 'Name must be at least 2 characters long').max(50, 'Name must be less than 50 characters long').required('Name is required'),
+    name: yup.string().required('Name is required').min(2, 'Name must be at least 2 characters long').max(50, 'Name must be less than 50 characters long'),
     email: yup.string().email('Invalid email').required('Email is required'),
-    password: yup.string().min(6, 'Password must be at least 6 characters long').max(255, 'Password must be less than 255 characters long').required('Password is required'),
-    confirmPassword: yup.string().oneOf([yup.ref('password')], 'Passwords must match').required('Confirm Password is required'),
+    password: yup.string().required('Password is required').min(6, 'Password must be at least 6 characters long').max(255, 'Password must be less than 255 characters long'),
+    confirmPassword: yup.string().oneOf([yup.ref('password')], 'Passwords must match'),
     acceptTerms: yup.bool().oneOf([true], 'Terms must be accepted').required('Terms must be accepted'),
 });
 
--- web-frontend/vitest.setup.ts
+++ web-frontend/vitest.setup.ts
@@ -1,1 +1,11 @@
 import '@testing-library/jest-dom/vitest';
+
+class ResizeObserver {
+    observe() {}
+    unobserve() {}
+    disconnect() {}
+}
+
+if (!window.ResizeObserver) {
+    window.ResizeObserver = ResizeObserver;
+}
